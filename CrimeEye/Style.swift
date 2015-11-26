71652a8cb865b72ade63ec6a1bfd0d5a128ac8f4
Gurpreet Paul <gurpreet@gurpreet.co>
2015-11-26 09:28:58 +0000

    Updated Style.swift to include 3 main colours and their respective lighter and darker shades

--- CrimeEye/Style.swift
+++ CrimeEye/Style.swift
@@ -11,11 +11,31 @@ import ChameleonFramework
 
 struct Style {
     
-    static var flatBlue = UIColor(hexString: "2980b9")
+    static var flatBlue1 = UIColor(hexString: "72B0D9")
+    static var flatBlue2 = UIColor(hexString: "4995C7")
+    static var flatBlue3 = UIColor(hexString: "2980b9")
+    static var flatBlue4 = UIColor(hexString: "096EB0")
+    static var flatBlue5 = UIColor(hexString: "07507F")
+
     
-    static var navbarBackground = flatBlue
+    static var flatGold1 = UIColor(hexString: "FFDE7F")
+    static var flatGold2 = UIColor(hexString: "FFD253")
+    static var flatGold3 = UIColor(hexString: "FFC82C")
+    static var flatGold4 = UIColor(hexString: "FFBC00")
+    static var flatGold5 = UIColor(hexString: "C79300")
     
-    static var viewBackground = flatBlue
+    static var flatRed1 = UIColor(hexString: "FFA07F")
+    static var flatRed2 = UIColor(hexString: "FF7F53")
+    static var flatRed3 = UIColor(hexString: "FF622C")
+    static var flatRed4 = UIColor(hexString: "FF4100")
+    static var flatRed5 = UIColor(hexString: "C73300")
+
     
-    static var fontColor = UIColor.whiteColor()
+    static var navbarBackground = flatBlue3
+    static var viewBackground   = flatBlue3
+    static var warningColor     = flatRed5
+    
+    static var sectionHeaders   = flatGold1
+
+    static var fontColor        = UIColor.whiteColor()
 }
