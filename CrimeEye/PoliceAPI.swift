c56553d8f639a7e75174c58b39e4e2bf4575338a
Gurpreet Paul <gurpreet@gurpreet.co>
2015-12-11 00:00:06 +0000

    Documented PoliceAPI

--- CrimeEye/PoliceAPI.swift
+++ CrimeEye/PoliceAPI.swift
@@ -3,21 +3,41 @@
 //  CrimeEye
 //
 //  Created by Gurpreet Paul on 25/11/2015.
-//  Copyright © 2015 Gurpreet Paul. All rights reserved.
+//  Copyright © 2015 Crime Eye. All rights reserved.
 //
 
 import Siesta
 
+/**
+ _PoliceAPI defines the main API that we use
+ to extract data from and display that data in the app.
+ */
+
 class _PoliceAPI: Service {
     
-    var lastUpdated: String = ""
+    var lastUpdated = ""
     
+    /**
+     Get a list of street level crimes.
+     - parameters:
+       - lat: Latitude of the user's current location.
+       - long: Longitude of the user's current location.
+     - returns: A Resource to monitor the full list of crimes.
+     */
     func getCrimes(lat: Double, long: Double) -> Resource {
         return resource("/crimes-street/all-crime")
                 .withParam("lat", "\(lat)")
                 .withParam("lng", "\(long)")
     }
     
+    /**
+     Get outcomes at a location and in a specific month.
+     - parameters:
+        - date: Date in YYYY-MM format.
+        - lat: Latitude of the user's current location.
+        - long: Longitude of the user's current location.
+     - returns: A Resource to monitor the full list of outcomes.
+     */
     func getOutcomes(date: String, lat: Double, long: Double) -> Resource {
         return resource("/outcomes-at-location")
             .withParam("date", date)
@@ -25,18 +45,31 @@ class _PoliceAPI: Service {
             .withParam("lng", "\(long)")
     }
     
+    /**
+     From a full date (YYYY-MM-DD) strip the last 3 characters.
+     - parameters:
+        - fullDate: Latitude of the user's current location.
+     - returns: A string of the form YYYY-MM
+     */
     func getYearAndMonth(fullDate: String) -> String {
         let truncated = fullDate.characters.dropLast(3)
         return "\(truncated)"
     }
     
+    /**
+     When was the API last updated?
+     - returns: A Resource that holds when the 
+     last updated date was.
+     */
     func getLastUpdated() -> Resource {
         return resource("/crime-last-updated")
     }
     
+    
     func getCrimesLastUpdated(lastUpdated: String) -> Resource {
         return resource("/crime-last-updated")
     }
+    
     
     init() {
         super.init(base: "https://data.police.uk/api")
@@ -45,4 +78,6 @@ class _PoliceAPI: Service {
     
 }
 
+// Here we define PoliceAPI which will mean it will 
+// be available across the app
 let PoliceAPI = _PoliceAPI()
