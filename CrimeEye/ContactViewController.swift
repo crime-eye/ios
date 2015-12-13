6ea3e3bd249038c76ac40a0033791d698ad08b20
Gurpreet Paul <gurpreet@gurpreet.co>
2015-12-13 22:37:43 +0000

    Added working mapview to neighbourhood contact view

--- CrimeEye/ContactViewController.swift
+++ CrimeEye/ContactViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Siesta
+import MapKit
 
 class ContactViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ResourceObserver {
 
@@ -15,6 +16,8 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
     @IBOutlet weak var websiteLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mapView: MKMapView!
+
     
     var links = [Link]()
     
@@ -43,6 +46,7 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.backgroundColor = Style.viewBackground
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         
         // Auto set the tableview's cells height
         self.tableView.rowHeight = UITableViewAutomaticDimension
@@ -70,6 +74,44 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
         }
     }
     
+    
+    /**
+     Locates where an address is and adds a marker to the map
+     - parameters:
+        - addr: Address to find
+        - type: E.g station
+     */
+    internal func locateAddress(addr: String, type: String) {
+        let geocoder = CLGeocoder()
+        
+        // Get the address string
+        geocoder.geocodeAddressString(addr, completionHandler: {(placemarks, error) -> Void in
+            if error != nil {
+                print("Error", error)
+            }
+            
+            // Get the first hit
+            if let placemark = placemarks?.first {
+                let coordinates: CLLocationCoordinate2D = placemark.location!.coordinate
+                
+                // Create a region and pass it to the Map View
+                var region = MKCoordinateRegion()
+                region.center.latitude = coordinates.latitude;
+                region.center.longitude = coordinates.longitude;
+                region.span.latitudeDelta = 0.004;
+                region.span.longitudeDelta = 0.004;
+                
+                // Create an annotation and also pass that
+                let ann = MKPointAnnotation()
+                ann.coordinate = coordinates
+                ann.title = "Nearest \(type)"
+                
+                self.mapView.setRegion(region, animated: true)
+                self.mapView.addAnnotation(ann)
+            }
+        })
+    }
+    
     // Has the resource changed, called as part of ResourceObserver
     func resourceChanged(resource: Resource, event: ResourceEvent) {
         
@@ -86,14 +128,22 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
         // Otherwise we can now use the latest data from
         // contact details
         if contactDetails?.latestData != nil {
+            
+            // Obtain correct info
             let tel = contactDetails!.json["contact_details"]["telephone"].stringValue
             let email = contactDetails!.json["contact_details"]["email"].stringValue
             let addr = contactDetails!.json["locations"][0]["address"].stringValue
+            let type = contactDetails!.json["locations"][0]["type"].stringValue
             
+            // Add the nearest police station to map
+            locateAddress(addr, type: type)
+            
+            // Update view's labels
             phoneLabel.text     = tel
             websiteLabel.text   = addr
             emailLabel.text     = email
             
+            // Create the array of links
             if let items = contactDetails!.json["links"].array {
                 self.links = []
                 for item in items {
@@ -119,28 +169,37 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
         return self.links.count
     }
     
+    // Here we are setting all the relevant fields and info
     func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath)
         -> UITableViewCell {
             
         let cell = tableView
-            .dequeueReusableCellWithIdentifier("LinkCell", forIndexPath: indexPath)
+            .dequeueReusableCellWithIdentifier("LinkCell",
+                                                forIndexPath: indexPath)
             as UITableViewCell
+            
+        // Get the title from the Link object this row points to
         let title = self.links[indexPath.row].title
         
+        // Set the title
         cell.textLabel?.text = title
         cell.backgroundColor = Style.viewBackground
         
         return cell
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    // Open up browser if clicked in a link
+    func tableView(tableView: UITableView,
+        didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let url = self.links[indexPath.row].url
         UIApplication.sharedApplication().openURL(NSURL(string: url)!)
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
+        // Force a call to the api that this resource points to, even
+        // if loadIfNeeded() is called
         neighbourhood?.invalidate()
         neighbourhood?.removeObservers(ownedBy: self)
         contactDetails?.removeObservers(ownedBy: self)
@@ -149,7 +208,6 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
